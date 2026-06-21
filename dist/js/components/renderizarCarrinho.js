e4e61abb2ebed04f93dff0187b2b172cea1655cb
Daniel <dijalmaduartefleitas0@gmail.com>
2026-06-21 12:27:40 -0400

    Refactor:Refatorando o sistema de arquivos que soma o valor total n carrinho para POO JS

--- dist/js/components/renderizarCarrinho.js
+++ dist/js/components/renderizarCarrinho.js
@@ -1,19 +1,19 @@
-function inicarRenderizacaoProdutosCarrinhos(){var e=document.querySelector(".valor-total");let n=JSON.parse(localStorage.getItem("produtosCarrinho"))||[],t=document.querySelector(".base-cards-carrinho"),r=document.querySelector(".botao-finalizar-compra");if(t.innerHTML="",n.length<1)t.innerHTML='<p class="text-center text-light mb-0">Nada no carrinho</p>',e.innerText="R$ 0",r.style.transform="translateY(120%)",setTimeout(function(){r.style.display="none"},200);else{r.style.display="flex",setTimeout(function(){r.style.transform="translateY(0)"},200),n.forEach(e=>{var r=document.createElement("div");r.innerHTML=`
-            <div class='card' data-id="${e.id}">
-                <img src='${e.imagem}'>
-                <div class='card-body'>
-                    <h5 class='card-title'>${e.nome}</h5>
-                    <h3 class="valor">${e.valor}</h3>
-                    <h5 class="mb-0 mt-3">Tamanho:</h5>
-                    <h4 class="tamanho">${e.tamanho}</h4>
-                    <h5 class="mb-0 mt-3">Cor:</h5>
-                    <h4 class="cor">${e.cor}</h4>
-                    <h5 class="mb-0 mt-3">Quantidade:</h5>
-                    <h4 class="quantidade">${e.quantidade}</h4>
-                </div>
-                <div class="botao-remover">
-                    <button class="botao-remover__botao">
-                        Remover
-                    </button>
-                </div>
-            </div>`,t.appendChild(r)});var o=document.querySelectorAll(".base-cards-carrinho .card");let a=0;o.forEach(e=>{var r=e.querySelector(".card-body .valor").innerText,e=e.querySelector(".card-body .quantidade").innerText,r=parseFloat(r.replace("R$ ","")),e=parseFloat(e),r=r*parseInt(e);a+=r}),e.innerText="R$ "+a.toFixed(2).replace(".",","),document.querySelectorAll(".botao-remover__botao").forEach(r=>{r&&r.addEventListener("click",e=>{e.stopPropagation();let o=r.closest(".card");o.style.transition="opacity 0.5s ease",o.style.opacity="0",setTimeout(function(){let r=o.querySelector(".card-body .cor").innerText,a=o.querySelector(".card-body .tamanho").innerText,t=o.dataset.id;var e=n.filter(e=>!(String(e.id)===String(t)&&String(e.tamanho)===String(a)&&String(e.cor)===String(r)));n=e,localStorage.setItem("produtosCarrinho",JSON.stringify(n)),inicarRenderizacaoProdutosCarrinhos()},500)})})}}export{inicarRenderizacaoProdutosCarrinhos};
+function inicarRenderizacaoProdutosCarrinhos(){new cardsCarrinho}class cardsCarrinho{constructor(){this.valorTotal=0,this.IndicadorTotal=document.querySelector(".valor-total"),this.produtosCarrinho=JSON.parse(localStorage.getItem("produtosCarrinho"))||[],this.containerCardsCarrinho=document.querySelector(".base-cards-carrinho"),this.botaoFinalizarCompra=document.querySelector(".botao-finalizar-compra"),this.containerCardsCarrinho.innerHTML="",this.verificaProdutosPresente(),this.desenharCardExistente(),this.criarQuantidadeValorECor(),this.ouvinteRemoverCard()}verificaProdutosPresente(){this.produtosCarrinho.length<1?(this.containerCardsCarrinho.innerHTML='<p class="text-center text-light mb-0">Nada no carrinho</p>',this.valorTotal=0,this.IndicadorTotal.innerText="R$ "+this.valorTotal,this.botaoFinalizarCompra.style.transform="translateY(120%)",setTimeout(()=>{this.botaoFinalizarCompra.style.display="none"},200)):(this.botaoFinalizarCompra.style.display="flex",setTimeout(()=>{this.botaoFinalizarCompra.style.transform="translateY(0)"},200))}desenharCardExistente(){this.produtosCarrinho.forEach(r=>{var a=document.createElement("div");a.innerHTML=`
+                    <div class='card' data-id="${r.id}">
+                        <img src='${r.imagem}'>
+                    <div class='card-body'>
+                        <h5 class='card-title'>${r.nome}</h5>
+                        <h3 class="valor">${r.valor}</h3>
+                        <h5 class="mb-0 mt-3">Tamanho:</h5>
+                        <h4 class="tamanho">${r.tamanho}</h4>
+                        <h5 class="mb-0 mt-3">Cor:</h5>
+                        <h4 class="cor">${r.cor}</h4>
+                        <h5 class="mb-0 mt-3">Quantidade:</h5>
+                        <h4 class="quantidade">${r.quantidade}</h4>
+                    </div>
+                    <div class="botao-remover">
+                        <button class="botao-remover__botao">
+                            Remover
+                        </button>
+                    </div>
+                    </div>`,this.containerCardsCarrinho.appendChild(a)})}criarQuantidadeValorECor(){var r=document.querySelectorAll(".base-cards-carrinho .card");let t=0;r.forEach(r=>{var a=r.querySelector(".card-body .valor").innerText,r=r.querySelector(".card-body .quantidade").innerText,a=parseFloat(a.replace("R$ ","")),r=parseFloat(r),a=a*parseInt(r);t+=a}),this.IndicadorTotal.innerText="R$ "+t.toFixed(2).replace(".",",")}ouvinteRemoverCard(){document.querySelectorAll(".botao-remover__botao").forEach(a=>{a&&a.addEventListener("click",r=>{r.stopPropagation();let e=a.closest(".card");e.style.transition="opacity 0.5s ease",e.style.opacity="0",setTimeout(()=>{let a=e.querySelector(".card-body .cor").innerText,t=e.querySelector(".card-body .tamanho").innerText,o=e.dataset.id;var r=this.produtosCarrinho.filter(r=>!(String(r.id)===String(o)&&String(r.tamanho)===String(t)&&String(r.cor)===String(a)));this.produtosCarrinho=r,localStorage.setItem("produtosCarrinho",JSON.stringify(this.produtosCarrinho)),inicarRenderizacaoProdutosCarrinhos()},500)})})}}export{inicarRenderizacaoProdutosCarrinhos};
--- src/js/components/renderizarCarrinho.js
+++ src/js/components/renderizarCarrinho.js
@@ -1,101 +1,120 @@
 export function inicarRenderizacaoProdutosCarrinhos() {
+    new cardsCarrinho
+}
+    class cardsCarrinho{
+        constructor(){
+        this.valorTotal = 0;
+        this.IndicadorTotal = document.querySelector(".valor-total");
+        this.produtosCarrinho = JSON.parse(localStorage.getItem("produtosCarrinho")) || [];
+        this.containerCardsCarrinho = document.querySelector(".base-cards-carrinho");
+        this.botaoFinalizarCompra = document.querySelector(".botao-finalizar-compra");
 
-    let valorTotal = 0;
-    const IndicadorTotal = document.querySelector(".valor-total");
-    let produtosCarrinho = JSON.parse(localStorage.getItem("produtosCarrinho")) || [];
-    const containerCardsCarrinho = document.querySelector(".base-cards-carrinho");
-    const botaoFinalizarCompra = document.querySelector(".botao-finalizar-compra");
+        this.containerCardsCarrinho.innerHTML = "";
 
+        this.verificaProdutosPresente();
+        this.desenharCardExistente();
+        this.criarQuantidadeValorECor();
+        this.ouvinteRemoverCard();
+
+        }
+        verificaProdutosPresente(){
+            if (this.produtosCarrinho.length < 1) {
+                this.containerCardsCarrinho.innerHTML = `<p class="text-center text-light mb-0">Nada no carrinho</p>`;
+                this.valorTotal = 0;
+                this.IndicadorTotal.innerText = `R$ ${this.valorTotal}`;
+                this.botaoFinalizarCompra.style.transform = "translateY(120%)";
+                    setTimeout(() => {
+                        this.botaoFinalizarCompra.style.display = "none";
+                    },200)
+            return;
+            }else{
+                this.botaoFinalizarCompra.style.display = "flex";
+                setTimeout(() => {
+                    this.botaoFinalizarCompra.style.transform = "translateY(0)";
+                },200)
+            }
+        }
+        desenharCardExistente(){
+            this.produtosCarrinho.forEach(dados => {
+                const cardProduto = document.createElement('div');
+                cardProduto.innerHTML = `
+                    <div class='card' data-id="${dados.id}">
+                        <img src='${dados.imagem}'>
+                    <div class='card-body'>
+                        <h5 class='card-title'>${dados.nome}</h5>
+                        <h3 class="valor">${dados.valor}</h3>
+                        <h5 class="mb-0 mt-3">Tamanho:</h5>
+                        <h4 class="tamanho">${dados.tamanho}</h4>
+                        <h5 class="mb-0 mt-3">Cor:</h5>
+                        <h4 class="cor">${dados.cor}</h4>
+                        <h5 class="mb-0 mt-3">Quantidade:</h5>
+                        <h4 class="quantidade">${dados.quantidade}</h4>
+                    </div>
+                    <div class="botao-remover">
+                        <button class="botao-remover__botao">
+                            Remover
+                        </button>
+                    </div>
+                    </div>`; 
     
-    containerCardsCarrinho.innerHTML = "";
+            this.containerCardsCarrinho.appendChild(cardProduto);
+            });
+        }
+        criarQuantidadeValorECor(){
+            const cardExistentes = document.querySelectorAll(".base-cards-carrinho .card");
 
-    if (produtosCarrinho.length < 1) {
-        containerCardsCarrinho.innerHTML = `<p class="text-center text-light mb-0">Nada no carrinho</p>`;
-        valorTotal = 0;
-        IndicadorTotal.innerText = `R$ ${valorTotal}`;
-        botaoFinalizarCompra.style.transform = "translateY(120%)";
-            setTimeout(function(){
-                botaoFinalizarCompra.style.display = "none";
-            },200)
-        return;
-    } 
-        botaoFinalizarCompra.style.display = "flex";
-        setTimeout(function(){
-            botaoFinalizarCompra.style.transform = "translateY(0)";
-        },200)
+            let somaDosValores = 0;
 
-    produtosCarrinho.forEach(dados => {
-        const cardProduto = document.createElement('div');
-        cardProduto.innerHTML = `
-            <div class='card' data-id="${dados.id}">
-                <img src='${dados.imagem}'>
-                <div class='card-body'>
-                    <h5 class='card-title'>${dados.nome}</h5>
-                    <h3 class="valor">${dados.valor}</h3>
-                    <h5 class="mb-0 mt-3">Tamanho:</h5>
-                    <h4 class="tamanho">${dados.tamanho}</h4>
-                    <h5 class="mb-0 mt-3">Cor:</h5>
-                    <h4 class="cor">${dados.cor}</h4>
-                    <h5 class="mb-0 mt-3">Quantidade:</h5>
-                    <h4 class="quantidade">${dados.quantidade}</h4>
-                </div>
-                <div class="botao-remover">
-                    <button class="botao-remover__botao">
-                        Remover
-                    </button>
-                </div>
-            </div>`; 
+            cardExistentes.forEach(card => {
+                const valor = card.querySelector(".card-body .valor").innerText;
+                const quantidade = card.querySelector(".card-body .quantidade").innerText;
+
+                const valorEmFloat = parseFloat(valor.replace("R$ ", ""));
+                let quantidadeEmFloat = parseFloat(quantidade);
+
+                const valorTotalDoCard = valorEmFloat * parseInt(quantidadeEmFloat);
+
+                somaDosValores += valorTotalDoCard; 
+            });
+            this.IndicadorTotal.innerText = `R$ ${somaDosValores.toFixed(2).replace('.', ',')}`;
+        }
+        ouvinteRemoverCard(){
+            const botaoRemover = document.querySelectorAll(".botao-remover__botao");
     
-        containerCardsCarrinho.appendChild(cardProduto);
-    });
+            botaoRemover.forEach(btnDeletar => {
+            if (btnDeletar) {
+                btnDeletar.addEventListener("click", (event) => {
+                    event.stopPropagation();
 
-    const cardExistentes = document.querySelectorAll(".base-cards-carrinho .card");
+                    const card = btnDeletar.closest(".card");
 
-    let somaDosValores = 0;
+                    card.style.transition = "opacity 0.5s ease";
+                    card.style.opacity = "0";
 
-    cardExistentes.forEach(card => {
-        const valor = card.querySelector(".card-body .valor").innerText;
-        const quantidade = card.querySelector(".card-body .quantidade").innerText;
+                    setTimeout(() => {
+                        const corProduto = card.querySelector(".card-body .cor").innerText;
+                        const tamanhoProduto = card.querySelector(".card-body .tamanho").innerText;
+                        const idProduto = card.dataset.id;
 
-        const valorEmFloat = parseFloat(valor.replace("R$ ", ""));
-        let quantidadeEmFloat = parseFloat(quantidade);
-
-        const valorTotalDoCard = valorEmFloat * parseInt(quantidadeEmFloat);
-
-        somaDosValores += valorTotalDoCard; 
-    });
-        IndicadorTotal.innerText = `R$ ${somaDosValores.toFixed(2).replace('.', ',')}`;
-
-        const botaoRemover = document.querySelectorAll(".botao-remover__botao");
-    
-    botaoRemover.forEach(btnDeletar => {
-        if (btnDeletar) {
-            btnDeletar.addEventListener("click", (event) => {
-                event.stopPropagation();
-
-                const card = btnDeletar.closest(".card");
-
-                card.style.transition = "opacity 0.5s ease";
-                card.style.opacity = "0";
-
-                setTimeout(function() {
-                    const corProduto = card.querySelector(".card-body .cor").innerText;
-                    const tamanhoProduto = card.querySelector(".card-body .tamanho").innerText;
-                    const idProduto = card.dataset.id;
-
-                    const novaArraySemObjeto = produtosCarrinho.filter(item => {
-                        return !(String(item.id) === String(idProduto) && 
+                        const novaArraySemObjeto = this.produtosCarrinho.filter(item => {
+                            return !(String(item.id) === String(idProduto) && 
                                 String(item.tamanho) === String(tamanhoProduto) && 
                                 String(item.cor) === String(corProduto));
                             });
                     
-                    produtosCarrinho = novaArraySemObjeto;
+                    this.produtosCarrinho = novaArraySemObjeto;
 
-                    localStorage.setItem("produtosCarrinho", JSON.stringify(produtosCarrinho));
+                    localStorage.setItem("produtosCarrinho", JSON.stringify(this.produtosCarrinho));
                 
                     inicarRenderizacaoProdutosCarrinhos();
-                }, 500);
-            });
+                    }, 500);
+                });
+            }
+        });
         }
-    });
-}
+        
+    }
+
+
+
+        
