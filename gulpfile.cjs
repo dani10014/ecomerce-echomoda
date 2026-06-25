703a1bf6cecbe271ab40338136ec274d4fe08f75
Daniel <dijalmaduartefleitas0@gmail.com>
2026-06-25 17:39:53 -0400

    Fix:adicionando mascara de senha

--- gulpfile.cjs
+++ gulpfile.cjs
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
 
 function compilaSass() {
     return gulp.src('src/sass/**/*.scss')
@@ -9,14 +10,16 @@ function compilaSass() {
 }
 function compilaJs(){
     return gulp.src('src/js/**/*.js')
+        .pipe(babel({
+            presets: ['@babel/preset-env'] // Transpila o JS moderno para ES5
+        }))
         .pipe(uglify())
         .pipe(gulp.dest('dist/js'));
+        
 }
-
 function watchFiles() {
     gulp.watch('src/sass/**/*.scss', compilaSass)
     gulp.watch('src/js/**/*.js', compilaJs)
-
 }
 
 exports.default = gulp.series(compilaSass,compilaJs,watchFiles);
