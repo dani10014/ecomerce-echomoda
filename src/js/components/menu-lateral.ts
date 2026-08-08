8fb007a5450a084884b1c451ae2805de909c976a
Daniel <dijalmaduartefleitas0@gmail.com>
2026-08-08 14:11:13 -0400

    inicio refatoracao

--- src/js/components/menu-lateral.ts
+++ src/js/components/menu-lateral.ts
@@ -1,13 +1,25 @@
 export function iniciarOuvinteMenuLateral(){
     new menuLateralBase()
 }
+    
+interface dados{
+    nome:string,
+    email:string,
+    numero:string,
+}
     class menuLateralBase{
+        menuLateral:HTMLElement;
+        btnMenuHamburguer:HTMLButtonElement;
+        botaoFechar:HTMLButtonElement;
+        backdropFilter:HTMLElement;
+        nomeClienteNoTopo:HTMLElement;
+
         constructor(){
-            this.btnMenuHamburguer = document.querySelector("#menu-hamburguer");
-            this.menuLateral = document.querySelector(".menu-lateral");
-            this.botaoFechar = document.querySelector("#botao-fechar");
-            this.backdropFilter = document.querySelector(".backdrop-glass");
-            this.nomeClienteNoTopo = document.querySelector("#menu-lateral__nome-cliente")
+            this.btnMenuHamburguer = document.querySelector("#menu-hamburguer") as HTMLButtonElement;
+            this.menuLateral = document.querySelector(".menu-lateral")as HTMLElement;
+            this.botaoFechar = document.querySelector("#botao-fechar") as HTMLButtonElement;
+            this.backdropFilter = document.querySelector(".backdrop-glass") as HTMLElement;
+            this.nomeClienteNoTopo = document.querySelector("#menu-lateral__nome-cliente") as HTMLElement;
 
             this.ouvinteBotaoMenuLateral();
             this.botaoFecharMenuLateral();
@@ -17,14 +29,14 @@ export function iniciarOuvinteMenuLateral(){
             this.btnMenuHamburguer.addEventListener("click",() => {
             this.menuLateral.style.display = "block";
         
-            const nomeCliente = JSON.parse(localStorage.getItem("Usuario"));
+            const nomeCliente = JSON.parse(localStorage.getItem("Usuario") || "{}") as dados ;
             
-            if(!nomeCliente){return}
+            if(!nomeCliente.nome){return}
             
             setTimeout(() => {
                 this.backdropFilter.style.display = "flex";
                 this.menuLateral.style.transform = "translateX(0)";
-                this.menuLateral.style.backdropfilter = "blur(1px)"
+                this.menuLateral.style.backdropFilter = "blur(1px)"
             },100)
 
             const {nome,...restoDados} = nomeCliente
